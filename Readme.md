641922f6d275ddef13a5747998b7442a0386cd80
Paul Freund <freund.paul@lvl3.org>
2013-04-07 17:39:29 +0200

    Imported project

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# WebLaunch #
+
+WebLaunch is an extension for the Kindle Touch ( and probably the Kindle PaperWhite too ) with JailBreak and the application launcher KUAL. It is designed to open a URL without the browser frame (the black status bar is still there) to make it look like a native application.
+
+## Installation ##
+
+* Make sure your Kindle Touch is already Jailbreaked
+* Make sure you have KUAL installed and the "extensions" folder exists 
+* Copy the WebLaunch folder to the "extensions" folder ( don't rename it without changing bin/start.sh first )
+* Copy the settings.example.cfg in the WebLaunch folder to settings.cfg and change its contents to fit your needs
+
+## Notes ##
+
+Special thanks go to the folks at mobileread.com for their great work opening up the Kindle.
---
+++ bin/start.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+###############################################################################
+## Configuration variables
+
+APP_FOLDER="/mnt/base-us/extensions/WebLaunch"
+
+SETTINGS_FILE="$APP_FOLDER/settings.cfg"
+
+APPREG="/var/local/appreg.db"
+
+APP_ID="com.PaulFreund.WebLaunch"
+APP_ID_STRING="'$APP_ID'"
+
+## WARNING - Changing the extension path after first run will result in errors,
+## the following commands delete old keys from the app registry
+# sqlite3 $APPREG "delete from handlerIds where handlerId=$APP_ID_STRING"
+# sqlite3 $APPREG "delete from properties where handlerId=$APP_ID_STRING"
+
+###############################################################################
+## Register application if neccessary
+
+#------------------------------------------------------------------------------
+# Application Id
+REG_ID=$(sqlite3 $APPREG "select handlerId from handlerIds where handlerId=$APP_ID_STRING")
+
+if [[ -z "$REG_ID" ]]
+then
+	echo "Application Id not registered, registering..."
+	sqlite3 $APPREG "INSERT INTO handlerIds VALUES ($APP_ID_STRING)"
+fi
+
+#------------------------------------------------------------------------------
+# Application command
+REG_COMMAND=$(sqlite3 $APPREG "select handlerId from properties where handlerId=$APP_ID_STRING and name='command'")
+
+if [[ -z "$REG_COMMAND" ]]
+then
+	echo "Application command not registered, registering..."
+	sqlite3 $APPREG "INSERT INTO properties (handlerId, name, value) VALUES ($APP_ID_STRING,'command','/usr/bin/mesquite -l $APP_ID -c $APP_FOLDER/bin/')"
+fi
+
+#------------------------------------------------------------------------------
+# Application unloadPolicy
+REG_UNLOADPOLICY=$(sqlite3 $APPREG "select handlerId from properties where handlerId=$APP_ID_STRING and name='unloadPolicy'")
+
+if [[ -z "$REG_UNLOADPOLICY" ]]
+then
+	echo "Application unloadPolicy not registered, registering..."
+	sqlite3 $APPREG "INSERT INTO properties (handlerId, name, value) VALUES ($APP_ID_STRING,'unloadPolicy','unloadOnPause')"
+fi
+
+###############################################################################
+## Get the settings
+SETTINGS=$(cat $SETTINGS_FILE)
+
+###############################################################################
+## Start the application
+lipc-set-prop com.lab126.appmgrd start app://$APP_ID?$SETTINGS
